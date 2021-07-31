5724439654786664c9fa2f2c700301360a641fb4
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-07-30 21:26:33 -0300

    Bug UUID

--- src/schemas/ChatRoom.ts
+++ src/schemas/ChatRoom.ts
@@ -1,5 +1,4 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { v4 as uuidv4 } from "uuid";
 import { User } from "./User";
 
 type ChatRoom = Document & {
@@ -15,8 +14,7 @@ const ChatRoomSchema = new Schema({
     }
   ],
   idChatRoom: {
-    type: String,
-    default: uuidv4()
+    type: String
   }
 });
 
--- src/services/CreateChatRoomService.ts
+++ src/services/CreateChatRoomService.ts
@@ -1,10 +1,11 @@
 import { injectable } from "tsyringe";
+import { v4 as uuidv4 } from "uuid";
 import { ChatRoom } from "../schemas/ChatRoom";
 
 injectable()
 class CreateChatRoomService {
   async execute(idUsers: string[]) {
-    const room = await ChatRoom.create({ idUsers });
+    const room = await ChatRoom.create({ idUsers, idChatRoom: uuidv4() });
 
     return room;
   }
